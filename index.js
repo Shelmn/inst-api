e7db269b68d8388bc7a179f062665fbfb4993827
Shelmn <iseefire3009@gmail.com>
2018-10-05 18:18:06 +0300

    second com

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let express = require('express');
 let app = express();
 let http = require('http');
 let fs = require('fs');
+app.use("/", express.static(__dirname));
 
-http.get("http://google.com", res => {
-    console.log(res.rawHeaders);
+app.get("/", function (req, res) {
+    res.sendFile("/index.html");
 });
+
+app.listen(process.env.PORT || 5000, () => console.log('server started on 5000 port'));
